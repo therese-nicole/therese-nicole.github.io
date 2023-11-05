9a59e575e56b3d7a629a862925fdac7fbf77a926
Therese Nicole Franklin <83608333+therese-nicole@users.noreply.github.com>
2023-11-05 09:50:23 -0800

    add website files

---
+++ main.js
@@ -0,0 +1,51 @@
+
+/// === sandwich topnav function === ///
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+/// === scroll to the top function === ///
+
+// Get the button
+let mybutton = document.getElementById("to-top"); 
+
+// When the user scrolls down 500 pixels from the top of the document, show the button
+window.onscroll = function() {scrollFunction()}; 
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+/* JS to get collapsible Abstracts to work */
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
